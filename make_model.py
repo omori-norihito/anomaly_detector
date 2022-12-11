3432dd20a4c62927bdcca952394f61b72820e8d9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 20:35:00 +0000

    Adding tarfile member sanitization to extractall()

--- make_model.py
+++ make_model.py
@@ -30,7 +30,26 @@ def download():
         url = f"https://www.mydrive.ch/shares/38536/3830184030e49fe74747669442f0f282/download/420937637-1629952063/{cls}.tar.xz"
         wget.download(url)
         with tarfile.open(f"{cls}.tar.xz") as tar:
-            tar.extractall(DATASETS_PATH)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, DATASETS_PATH)
         os.remove(f"{cls}.tar.xz")
         print("")  # force newline
 
